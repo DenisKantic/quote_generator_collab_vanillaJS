c1028f340f2fcf62931439fc9036639409c1900d
nedimmujcinovic <nedimmujcinovic1@gmail.com>
2022-09-20 21:37:19 +0200

    I made some logic in js file

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'd21bfe54c4msh420a0871374d5d3p188258jsn3d48e86f4482',
-		'X-RapidAPI-Host': 'marvel-quote-api.p.rapidapi.com'
-	}
-};
+const quote = document.getElementsByClassName('quote')[0];
+const nameOfAuthor = document.getElementsByClassName('nameOfAuthor')[0];
+const btn = document.getElementsByClassName('btn')[0];
 
-fetch('https://marvel-quote-api.p.rapidapi.com/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const displayInfo = (data) => {
+	quote.innerText = data.Quote;
+	nameOfAuthor.innerText = data.Speaker;
+}
+
+btn.addEventListener("click", () => {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'd21bfe54c4msh420a0871374d5d3p188258jsn3d48e86f4482',
+			'X-RapidAPI-Host': 'marvel-quote-api.p.rapidapi.com'
+		}
+	};
+
+	fetch('https://marvel-quote-api.p.rapidapi.com/', options)
+		.then(response => response.json())
+		.then(response => displayInfo(response))
+		.catch(err => console.error(err));
+});
